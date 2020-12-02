b47da38f3e82220482dd89e2a02147cbc37fc77e
Shashank Nambi <59937058+ShashankNambi1998@users.noreply.github.com>
2020-12-02 13:24:34 -0600

    Created initial lexer file
    Created file, and mapped out possible functions required.

---
+++ lexer.py
@@ -0,0 +1,21 @@
+class Lexer:
+    def _init_(self, input):
+        pass
+
+    def getToken(self):
+        pass
+
+    def nextChar(self):
+        pass
+
+    def lookNext(self):
+        pass
+
+    def abort(self):
+        pass
+
+    def ignoreWhiteSpace(self):
+        pass
+
+    def ignoreComments(self):
+        pass
